ba190d1cd23079d352f5b9bfb3ba7751a27c3900
Lok-Wong <57250089+Lok-Wong@users.noreply.github.com>
2024-07-31 14:38:29 +0800

    update

--- app/RsurveyInfo/page.js
+++ app/RsurveyInfo/page.js
@@ -7,14 +7,14 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import TextField from '@mui/material/TextField';
 import { useRouter } from 'next/navigation';
 import FormHelperText from '@mui/material/FormHelperText';
 import Autocomplete from '@mui/material/Autocomplete';
-import { schoolName, school_type, region, schoolAddress, schoolLevel, levelType, schoolType } from '../schoolData'
 import LinearProgresss from '@/app/utils/progress';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+import Slider from '@mui/material/Slider';
+import { Box } from '@mui/material';
 
 function App() {
     const router = useRouter();
@@ -27,6 +27,7 @@ function App() {
         gender: 999,
         age: 999,
         workingStatus: 999,
+        degree: 999,
         startTime: new Date(),
     }
 
@@ -99,39 +100,6 @@ function App() {
     }
 
     const handleChange = (event) => {
-
-        if (event.target.name == "schoolArea") {
-            setSurvey((prevState) => (
-                {
-                    ...prevState,
-                    schoolArea: event.target.value,
-                    schoolName: 999,
-                    schoolType: 999,
-                    classLevel: 999,
-                    levelType: 999,
-                }
-            )
-            )
-            setShValue("")
-            setShInputValue("")
-            setStValue("")
-            setSchoolNameSelectType("")
-            return
-        }
-
-        if (event.target.name == "classLevel") {
-            setSurvey((prevState) => (
-                {
-                    ...prevState,
-                    classLevel: event.target.value,
-                    levelType: 999
-                }
-            )
-            )
-            return
-        }
-
-
         const objectName = event.target.name
         setSurvey((prevState) => (
             {
@@ -143,15 +111,6 @@ function App() {
 
         )
     };
-
-    const getSchoolTypeFromSchoolName = () => {
-        setSurvey((prevState) => (
-            {
-                ...prevState,
-                schoolType: schoolType[survey.schoolName]
-            }
-        ))
-    }
 
     const [isClient, setIsClient] = React.useState(false)
 
@@ -311,9 +270,66 @@ function App() {
         }
     }, [survey]);
 
-    React.useEffect(() => {
-        getSchoolTypeFromSchoolName()
-    }, [survey.schoolName])
+
+    const getWorkingStatusContent = () => {
+        if (survey.workingStatus === "就學") {
+            return (
+                <div className={styles.question}>
+                    <FormControl>
+                        <FormLabel id="degree-label"><h3>程度：</h3></FormLabel>
+                        <RadioGroup
+                            aria-labelledby="degree-label"
+                            name="degree"
+                            onChange={handleChange}
+                            value={survey.degree}
+                        >
+                            <FormControlLabel sx={{ color: "black" }} value="大專院校及以上學校" control={<Radio />} label="大專院校及以上學校" />
+                            <FormControlLabel sx={{ color: "black" }} value="職業學校學生" control={<Radio />} label="職業學校學生" />
+                            <FormControlLabel sx={{ color: "black" }} value="中學" control={<Radio />} label="中學" />
+                            <FormControlLabel sx={{ color: "black" }} value="小學" control={<Radio />} label="小學" />
+                            <FormControlLabel sx={{ color: "black" }} value="幼稚園" control={<Radio />} label="幼稚園" />
+
+                        </RadioGroup>
+                        <FormHelperText sx={{ color: 'red' }}>{helpText.degree}</FormHelperText>
+                    </FormControl>
+                </div>
+            )
+        }
+
+        if (survey.workingStatus === "就業") {
+            return (
+                <div className={styles.question}>
+                    <FormControl>
+                        <FormLabel id="job-categories-label"><h3>程度：</h3></FormLabel>
+                        <RadioGroup
+                            aria-labelledby="job-categories-label"
+                            name="jobCategories"
+                            onChange={handleChange}
+                            value={survey.jobCategories}
+                        >
+                            <FormControlLabel sx={{ color: "black" }} value="醫療衛生及社會福利 " control={<Radio />} label="醫療衛生及社會福利" />
+                            <FormControlLabel sx={{ color: "black" }} value="文娛博彩及其他服務業" control={<Radio />} label="文娛博彩及其他服務業" />
+                            <FormControlLabel sx={{ color: "black" }} value="批發及零售業" control={<Radio />} label="批發及零售業" />
+                            <FormControlLabel sx={{ color: "black" }} value="教育" control={<Radio />} label="教育" />
+                            <FormControlLabel sx={{ color: "black" }} value="公共行政及社保事務" control={<Radio />} label="公共行政及社保事務" />
+                            <FormControlLabel sx={{ color: "black" }} value="運輸、倉儲及通訊業 " control={<Radio />} label="運輸、倉儲及通訊業" />
+                            <FormControlLabel sx={{ color: "black" }} value="酒店" control={<Radio />} label="酒店" />
+                            <FormControlLabel sx={{ color: "black" }} value="金融業 " control={<Radio />} label="金融業" />
+                            <FormControlLabel sx={{ color: "black" }} value="不動產及工商服務業" control={<Radio />} label="不動產及工商服務業" />
+                            <FormControlLabel sx={{ color: "black" }} value="水電及氣體生產供應業" control={<Radio />} label="水電及氣體生產供應業" />
+                            <FormControlLabel sx={{ color: "black" }} value="飲食業" control={<Radio />} label="飲食業" />
+                            <FormControlLabel sx={{ color: "black" }} value="製造業" control={<Radio />} label="製造業" />
+                            <FormControlLabel sx={{ color: "black" }} value="家務工作" control={<Radio />} label="家務工作" />
+                            <FormControlLabel sx={{ color: "black" }} value="建築業" control={<Radio />} label="建築業" />
+                            <FormControlLabel sx={{ color: "black" }} value="其他" control={<Radio />} label="其他" />
+
+                        </RadioGroup>
+                        <FormHelperText sx={{ color: 'red' }}>{helpText.jobCategories}</FormHelperText>
+                    </FormControl>
+                </div>
+            )
+        }
+    }
 
 
 
@@ -363,74 +379,86 @@ function App() {
                             </FormControl>
                         </div>
 
-                        <div>
-                            <div className={styles.question}>
-                                <FormControl>
-                                    <FormLabel id="person-od-type-label"><h3>2.1) 居住地點（地標）：</h3></FormLabel>
+                        {
+                            survey.personOdType == "旅客" ?
+                                <p>不符合，感謝參與。</p>
+                                :
+                                <div>
+                                    <div className={styles.question}>
+                                        <FormControl>
+                                            <FormLabel id="person-od-type-label"><h3>2.1) 居住地點（地標）：</h3></FormLabel>
 
-                                    <p>
-                                        打開地圖，選擇您的居住地點
-                                    </p>
+                                            <p>
+                                                打開地圖，選擇您的居住地點
+                                            </p>
 
-                                    <FormLabel id="person-od-type-label"><h3>2.2) 工作地點（地標）：</h3></FormLabel>
+                                            <FormLabel id="person-od-type-label"><h3>2.2) 工作地點（地標）：</h3></FormLabel>
 
-                                    <p>
-                                        打開地圖，選擇您的居住地點
-                                    </p>
-                                </FormControl>
-                            </div>
+                                            <p>
+                                                打開地圖，選擇您的居住地點
+                                            </p>
+                                        </FormControl>
+                                    </div>
 
-                            <div key={7} className={styles.question}>
-                                <FormControl>
-                                    <FormLabel id="age-label"><h3>3)  年齡：</h3></FormLabel>
-                                    <RadioGroup
-                                        aria-labelledby="age-label"
-                                        name="age"
-                                        onChange={handleChange}
-                                        value={survey.age}
-                                    >
-                                        <FormControlLabel sx={{ color: "black" }} value="0~4歲" control={<Radio />} label="0~4歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="5~9歲" control={<Radio />} label="5~9歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="10~14歲" control={<Radio />} label="10~14歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="15~19歲" control={<Radio />} label="15~19歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="20-24歲" control={<Radio />} label="20-24歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="25-29歲" control={<Radio />} label="25-29歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="30-34歲" control={<Radio />} label="30-34歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="35-39歲" control={<Radio />} label="35-39歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="40-44歲" control={<Radio />} label="40-44歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="45-49歲" control={<Radio />} label="45-49歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="50-54歲" control={<Radio />} label="50-54歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="55-59歲" control={<Radio />} label="55-59歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="60-64歲" control={<Radio />} label="60-64歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="65-69歲" control={<Radio />} label="65-69歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="70-74歲" control={<Radio />} label="70-74歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="75-79歲" control={<Radio />} label="75-79歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value="80-84歲" control={<Radio />} label="80-84歲" />
-                                        <FormControlLabel sx={{ color: "black" }} value=">=85歲" control={<Radio />} label=">=85歲" />
-                                    </RadioGroup>
-                                    <FormHelperText sx={{ color: 'red' }}>{helpText.age}</FormHelperText>
-                                </FormControl>
-                            </div>
+                                    <div key={7} className={styles.question}>
+                                        <FormControl>
+                                            <FormLabel id="age-label"><h3>3)  年齡：</h3></FormLabel>
+                                            <Box sx={{width:"400%"}}>
+                                                <Slider valueLabelDisplay="auto" aria-label="age" name='age' value={survey.age} onChange={handleChange} />
+                                            </Box>
+                                            {/* <RadioGroup
+                                                aria-labelledby="age-label"
+                                                name="age"
+                                                onChange={handleChange}
+                                                value={survey.age}
+                                            >
+                                                <FormControlLabel sx={{ color: "black" }} value="0~4歲" control={<Radio />} label="0~4歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="5~9歲" control={<Radio />} label="5~9歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="10~14歲" control={<Radio />} label="10~14歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="15~19歲" control={<Radio />} label="15~19歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="20-24歲" control={<Radio />} label="20-24歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="25-29歲" control={<Radio />} label="25-29歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="30-34歲" control={<Radio />} label="30-34歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="35-39歲" control={<Radio />} label="35-39歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="40-44歲" control={<Radio />} label="40-44歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="45-49歲" control={<Radio />} label="45-49歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="50-54歲" control={<Radio />} label="50-54歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="55-59歲" control={<Radio />} label="55-59歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="60-64歲" control={<Radio />} label="60-64歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="65-69歲" control={<Radio />} label="65-69歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="70-74歲" control={<Radio />} label="70-74歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="75-79歲" control={<Radio />} label="75-79歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value="80-84歲" control={<Radio />} label="80-84歲" />
+                                                <FormControlLabel sx={{ color: "black" }} value=">=85歲" control={<Radio />} label=">=85歲" />
+                                            </RadioGroup> */}
+                                            <FormHelperText sx={{ color: 'red' }}>{helpText.age}</FormHelperText>
+                                        </FormControl>
+                                    </div>
 
-                            <div key={8} className={styles.question}>
-                                <FormControl>
-                                    <FormLabel id="working-status-label"><h3>4)  就業／就學情況是（擇一主要身份）：</h3></FormLabel>
-                                    <RadioGroup
-                                        id="workingStatus"
-                                        aria-labelledby="working-status-label"
-                                        name="workingStatus"
-                                        onChange={handleChange}
-                                        value={survey.workingStatus}
-                                    >
-                                        <FormControlLabel sx={{ color: "black" }} value="就學" control={<Radio />} label="就學" />
-                                        <FormControlLabel sx={{ color: "black" }} value="就業" control={<Radio />} label="就業" />
-                                    </RadioGroup>
-                                    <FormHelperText sx={{ color: 'red' }}>{helpText.workingStatus}</FormHelperText>
-                                </FormControl>
-                            </div>
-                        </div>
+                                    <div key={8} className={styles.question}>
+                                        <FormControl>
+                                            <FormLabel id="working-status-label"><h3>4)  就業／就學情況是（擇一主要身份）：</h3></FormLabel>
+                                            <RadioGroup
+                                                id="workingStatus"
+                                                aria-labelledby="working-status-label"
+                                                name="workingStatus"
+                                                onChange={handleChange}
+                                                value={survey.workingStatus}
+                                            >
+                                                <FormControlLabel sx={{ color: "black" }} value="就學" control={<Radio />} label="就學" />
+                                                <FormControlLabel sx={{ color: "black" }} value="就業" control={<Radio />} label="就業" />
+                                                <FormControlLabel sx={{ color: "black" }} value="待業" control={<Radio />} label="待業" />
+                                                <FormControlLabel sx={{ color: "black" }} value="無業（家庭主婦／退休）" control={<Radio />} label="無業（家庭主婦／退休）" />
 
+                                            </RadioGroup>
+                                            <FormHelperText sx={{ color: 'red' }}>{helpText.workingStatus}</FormHelperText>
+                                        </FormControl>
+                                    </div>
 
+                                    {getWorkingStatusContent()}
+                                </div>
+
+                        }
 
                     </div>
                     :
@@ -448,7 +476,12 @@ function App() {
 
                     <Button className={styles.buttonStyle}
                         onClick={handleNextButton}>
-                        下一頁
+                        {
+                            survey.personOdType == "旅客" ?
+                                "完成"
+                                :
+                                "下一頁"
+                        }
                     </Button>
                 </div>
             </div>
